1a0445a71e932b04e194cce2f60c20b3fa877b20
ali_hassam <alykhanhssm@gmail.com>
2019-02-12 22:55:20 -0700

    Update createNumberArrayTest.java

--- tests/createNumberArrayTest.java
+++ tests/createNumberArrayTest.java
@@ -15,6 +15,8 @@ public class createNumberArrayTest {
 	private double data[] = {0, 1, 2, 3, 4, 5};
 	private double emptyData[] = {};
 	private double nullData[];
+
+	private Number[] expected = {0, 1, 2, 3, 4, 5};
 	
 	@Before
 	public void setUp() throws Exception {
@@ -35,6 +37,12 @@ public class createNumberArrayTest {
 	@Test (expected = IllegalArgumentException.class)
 	public void testNullData() {
 		nullDataTest = DataUtilities.createNumberArray(nullData);
+	}
+	@Test
+	public void testExpectedArray()
+	{
+		boolean result = expected.equals(realDataTest);
+		assertTrue(result);
 	}
 	
 }
